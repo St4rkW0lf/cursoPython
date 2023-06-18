302e00a530c41a1961c1bc4977208655105a689d
stark <55291037+St4rkW0lf@users.noreply.github.com>
2023-06-18 16:38:09 -0300

    🥤 Refri em conta

---
+++ aula6/tipos_de_dados.py
@@ -0,0 +1,10 @@
+var_string = "Fulano da Silva"
+var_int = 25
+
+print(var_string, var_int)
+
+var_float = 2.50
+var_bool = 'true'
+
+print("Preço atual do refrigerante: ", var_float)
+print("Refrigerante tá em conta? ", var_bool)
